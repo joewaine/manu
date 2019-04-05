0ecd5e577cce56130ee7d7df4017df58eb98b83b
Joe Waine <joseph@comradeagency.com>
2019-04-05 16:26:28 -0700

    fixing diamond

--- src/components/navigationIcons/newNavigationDesktop10.vue
+++ src/components/navigationIcons/newNavigationDesktop10.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+<div class="svg-container">
 		
 <svg id="pentagonOutlinedDesktop" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 500.24 427.64" style="enable-background:new 0 0 500.24 427.64;" xml:space="preserve">
